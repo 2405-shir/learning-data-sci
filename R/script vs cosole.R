dabde8ccb56baa64ffad6dbc85b053fcf8b95db2
shir-bop <73941163+shir-bop@users.noreply.github.com>
2020-12-21 21:31:34 +1000

    Create script vs cosole.R

---
+++ R/script vs cosole.R
@@ -0,0 +1,6 @@
+console easy for one lined code
+script for long code
+script history is backed up console isnt
+code easier to execute in script
+editor puts red line on syntax errors
+less risk in using script
