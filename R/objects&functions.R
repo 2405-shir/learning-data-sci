7428c1f9027267838a099a510f7839e2dcfa35be
shir-bop <73941163+shir-bop@users.noreply.github.com>
2020-12-21 20:05:00 +1000

    Create objects&functions.R

---
+++ R/objects&functions.R
@@ -0,0 +1,14 @@
+objects are like variables
+how you make objects
+object name >- values
+bob*2
+[1] 14
+> print(bob)
+[1] 7
+lets create vector with values of 1 to ten
+a <- 1:10
+> print(a)
+ [1]  1  2  3  4  5  6  7  8  9 10
+> a*2
+ [1]  2  4  6  8 10 12 14 16 18 20
+> multiplies everything
