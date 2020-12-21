34e38eea78336abcd6cd80af15a92da66e21e5c0
shir-bop <73941163+shir-bop@users.noreply.github.com>
2020-12-21 20:42:57 +1000

    Create func.R

---
+++ R/func.R
@@ -0,0 +1,26 @@
+object is the baby
+func is the mom
+object<-function()
+to run a func
+function.name(data you want func to operate on)
+to round-round number in parathesis
+> round(2,4721)
+[1] 2
+use mean func for mean
+ mean(1:10)
+[1] 5.5
+> mean(a)
+[1] 5.5
+what we pass in func is called argument
+passing data for example
+data can be raw data
+our 1:10 case
+or object
+like a or bob
+or result of other func
+round(mean(a))
+[1] 6
+save round result as b
+ b<-round(mean(a))
+> print(b)
+[1] 6
