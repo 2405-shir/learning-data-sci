3852135a61487545cf561a430069b48a7b48a67a
shir-bop <73941163+shir-bop@users.noreply.github.com>
2020-12-21 20:57:40 +1000

    Create func and arg.R

---
+++ R/func and arg.R
@@ -0,0 +1,10 @@
+func can take more than one argument
+you can tell what type of argument a function takes
+by doing
+args(functions.name)
+> args(round)
+function (x, digits = 0) 
+NULL
+we can round to specifc number too by setting it in digits
+round(2.5774834738,digits=2)
+[1] 2.58
