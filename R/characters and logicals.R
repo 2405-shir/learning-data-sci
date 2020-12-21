ba8b9b1ebb8b51cd4f00123b31344d8805e81534
shir-bop <73941163+shir-bop@users.noreply.github.com>
2020-12-21 20:24:34 +1000

    Create characters and logicals.R

---
+++ R/characters and logicals.R
@@ -0,0 +1,13 @@
+characters
+vectors that can store text data
+you put them in ""
+aything inside "" is string
+> char<-c("hi","hoi")
+> typeof(char)
+[1] "character"
+logical vectors store boolean data
+shortforms are T&F
+> SPOCK<-c(T,F,T,F)
+> typeof(SPOCK)
+[1] "logical"
+R is very case sensitive 
