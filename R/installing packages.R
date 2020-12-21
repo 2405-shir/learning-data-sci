7cde0201c2e4d2cdc0ae23727bd92d8196c6a89b
shir-bop <73941163+shir-bop@users.noreply.github.com>
2020-12-21 19:57:31 +1000

    Create installing packages.R

---
+++ R/installing packages.R
@@ -0,0 +1,16 @@
+
+
+> 1st way-use menu
+
+> 2nd way-command
+
+> install.packages(name of packages)
+
+> install.packages("ggplot2")
+
+> like string between ""
+> library(ggplot2)
+> no parathesis
+> if you dont know which package you need
+
+> go r task views
