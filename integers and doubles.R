5f2917aa29444556e522299a646c17216b3d90c1
shir-bop <73941163+shir-bop@users.noreply.github.com>
2020-12-21 20:14:06 +1000

    Create integers and doubles.R

---
+++ integers and doubles.R
@@ -0,0 +1,19 @@
+vectors are seq of data elements of same type
+a and bob is vector
+how to check if they were vector
+is.vector(name of var)
+> is.vector(bob)
+[1] TRUE
+> is.vector(a)
+[1] TRUE
+both arrays and single values are stored as vector
+can create vector by creating
+c()
+can thik of as combine
+int<-c(5L,6L,7L,8L)
+to save data type as integers 
+add L after each value
+to find type
+typeof(name)
+typeof(int)
+[1] "integer"
