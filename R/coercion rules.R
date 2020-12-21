fd8a876b77b0088845baae25ddf64122dcc7edbc
shir-bop <73941163+shir-bop@users.noreply.github.com>
2020-12-21 20:29:21 +1000

    Create coercion rules.R

---
+++ R/coercion rules.R
@@ -0,0 +1,5 @@
+all vector elements must be of same type
+if vector has atleast one string element in it all other stuff will be converted to strings
+if a vector has only logical and numerical elements,all logicals will be converted to numbers
+True will become 1
+false will become 0
