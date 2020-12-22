d49a9ef077014db6c8be6e7bb71edf4b99bac6c3
shir-bop <73941163+shir-bop@users.noreply.github.com>
2020-12-22 14:38:28 +1000

    Create 1.database basics.sql

---
+++ SQL/1.Introduction/1.database basics.sql
@@ -0,0 +1,28 @@
+databases everywhere whether phone directory or googling stuff
+database? 
+database isnt software that runs database thats DBMS
+Its like filing cabinet for data,Structured place to store and manage
+what manages database is DBMS
+DMBS
+database management software
+like sql
+filefolders-table
+table is very structured only stores single type of data
+within tables theres coloumns
+coloumns are specific part of info stored like product table product price and name
+every coloumn needs to have a name
+and every coloumn needs to have a type
+any data you want to acess should be stored in coloumns
+individual records stored in rows
+individual paper in file folder
+relational database
+data is broken into lots of individual parts
+each stored in diff tables
+and theyre connected to each other
+these connections called relationships
+customer>order>orderitems
+relationships created using keys
+primary key
+a way to uniquely identify every sigle row in table like id that will never change
+foreign key
+used to link 2 tables to one another
